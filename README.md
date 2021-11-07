d473ff219babd917a5f6e24946eca9bceb649251
vlaskinmac <kangelina86@mail.ru>
2021-11-07 17:22:38 +0300

    - Created README.md - Added requirements.txt - Reformatted imports - Created defines_timeout_user() - Removed prints to defines_timeout_user() - Added loggings - Created constant 'CHAT_ID' to main() - Created notification logic in defines_timeout_user()

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# Space-photos
+# Space photos - Devman
+
+**Script sends a photo of the planet earth to the telegram channel.**
+
+
+This script is written as part of the task of the courses [Devman](https://dvmn.org).
+
+- When the code is run, photos are sent to the specified telegram channel. 
+
+- By default with a period of once a day. Or in a user-defined period. 
+
+- The photos were obtained from the [NASA](https://www.nasa.gov) website.
+
+- Photos in real color.
+
+
+<img src="https://user-images.githubusercontent.com/78322994/140647471-a178cbd6-4d2a-4387-8db4-7c74b344a680.png" alt="drawing" width="200"/> <img src="https://user-images.githubusercontent.com/78322994/140647474-46b29d3f-5227-4c11-b3c1-67a0ba0678eb.png" alt="drawing" width="200"/> <img src="https://user-images.githubusercontent.com/78322994/140647475-18735c1d-5dbb-44e2-a9a0-35041bb8e7a1.png" alt="drawing" width="200"/> <img src="https://user-images.githubusercontent.com/78322994/140647478-6d43996f-9208-4b7f-a737-93e9a2d64605.png" alt="drawing" width="200"/> <img src="https://user-images.githubusercontent.com/78322994/140647479-1d896c98-6e9a-4a4b-b528-86ce3bf7ab36.png" alt="drawing" width="200"/> <img src="https://user-images.githubusercontent.com/78322994/140647481-1d167daf-078d-4c9c-b5a7-11c59b5509ca.png" alt="drawing" width="200"/> <img src="https://user-images.githubusercontent.com/78322994/140647483-b4ba5081-8a26-4004-a7aa-e048115f9454.png" alt="drawing" width="200"/> <img src="https://user-images.githubusercontent.com/78322994/140647484-bbbf9e1b-2b7c-4493-a6c8-bdf88304f7f3.png" alt="drawing" width="200"/> 
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Python Version
+
+Python 3.6 and later.
+
+### Installing
+
+To install the software, you need to install the dependency packages from the file: **requirements.txt**.
+
+Perform the command:
+
+```
+
+pip3 install -r requirements.txt
+
+```
+
+## Variables
+
+- Сhat id must be registered in the **CHAT_ID** constant.
+
+## Getting API key
+
+**API key NASA**
+
+- To get the API key. You need to log in to the API service NASA link: [`API-NASA`](https://api.nasa.gov/).
+- On the main page, fill out the form and generate API key.
+
+**API key Telegram bot**
+
+- Go to Telegram. 
+- Call the system bot by typing in the search: [@BotFather](https://telegram.me/BotFather) 
+- Enter the command:
+```
+/token
+```
+
+
+### Connecting the API key
+
+You need to create a `.env` file and write all sensitive data into it, like this:
+
+```python
+API_KEY_NASA="272a05d39ec46fdac5be4ac7be45f3f"
+API_KEY_BOT="2127492642:AAFC4-Ey3WTtFNCcSzbDN7Z7y1ePaw8IbTU"
+```
+
+## Launch code
+#### Arguments
+- Set the update period in hours use arguments: **-hh** or **--hours**
+- Set the update period in seconds use arguments: **-s** or **--seconds**
+- To call help, use the required arguments **-h** or **--help**
+
+**Examples of commands:**
+
+```python
+$ python photos_space.py --seconds 30
+```
+
+```python
+$ python photos_space.py --hours 4
+```
+
+- Running code without arguments the default period is once every 24 hours:
+```python
+$ python photos_space.py 
+```
+
+## Authors
+
+**vlaskinmac**  - [GitHub-vlaskinmac](https://github.com/vlaskinmac/)
+
+
--- photos_space.py
+++ photos_space.py
@@ -1,104 +1,149 @@
+import argparse
+import datetime
 import logging
 import os
-
-import requests
-
-from dotenv import load_dotenv
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import time
 from urllib.parse import urlparse
 
+import requests
+import telegram
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-
-updater = Updater(token="2127492642:AAFC4-Ey3WTtFNCcSzbDN7Z7y1ePaw8IbTU", use_context=True)
-dispatcher = updater.dispatcher
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+from telegram import TelegramError
+from dotenv import load_dotenv
 
 
-start_handler = CommandHandler("start", start)
-dispatcher.add_handler(start_handler)
-
-
-def hi(update, context):
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-hi_handler = CommandHandler("hi", hi)
-dispatcher.add_handler(hi_handler)
-
-
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
-
-def caps(update, context):
-    text_caps = " ".join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-
-caps_handler = CommandHandler("caps", caps)
-dispatcher.add_handler(caps_handler)
-
-
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-
-
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
-
-updater.start_polling()
-# updater.stop()
-
-
-def get_container_links(url):
+def get_container_links(url, token):
     data = {"api_key": token}
     response = requests.get(url, params=data)
     response.raise_for_status()
+    logging.warning(response.status_code)
     return response.json()
 
 
-def fetch_spacex_last_launch(dir_name):
-    container_links = get_container_links(url=url_archive)
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-    file_path = os.path.abspath(dir_name)
+def fetch_spacex_last_launch(*args, **kwargs):
+    container_links = get_container_links(url=kwargs["url_archive"], token=kwargs["token"])
+    if not container_links:
+        logging.warning("Нет данных")
+        print("Нет данных")
+        exit()
+    if not os.path.exists(kwargs["dir_name"]):
+        os.mkdir(kwargs["dir_name"])
+    file_path = os.path.abspath(kwargs["dir_name"])
     for image_url_number, image_url in enumerate(container_links):
-        data = {"api_key": token}
+        data = {"api_key": kwargs["token"]}
         url_image = f"https://api.nasa.gov/EPIC/archive/natural/" \
-                    f"{year}/{month}/{day}/png/{image_url['image']}.png"
+                    f"{kwargs['year']}/{kwargs['month']}/{kwargs['day']}/png/{image_url['image']}.png"
         response_image = requests.get(url_image, params=data)
         response_image.raise_for_status()
-
+        logging.warning(response_image.status_code)
         cut_path_url = urlparse(url_image)
         name_file_url = os.path.split(cut_path_url.path)
         tail_url = os.path.splitext(name_file_url[-1])[-1]
+
         with open(f"{file_path}/{image_url_number}{tail_url}", "wb") as file:
             file.write(response_image.content)
             print(f"{image_url_number + 1} - картинка загружена")
+    return file_path
+
+
+def publish_photo(*args, **kwargs):
+    file_path = fetch_spacex_last_launch(*args, **kwargs)
+    file_images = os.listdir(file_path)
+    token_bot = kwargs["token_bot"]
+    bot = telegram.Bot(token=token_bot)
+
+    while True:
+        try:
+            bot.send_message(text="Добрый день!", chat_id=kwargs["chat_id"])
+            bot.send_message(text=f"Фотки за {kwargs['today_date']} число!", chat_id=kwargs["chat_id"])
+            count = 0
+            for image in file_images:
+                count += 1
+                file_images_nasa = os.path.join(file_path, image)
+                bot.send_message(text=f"Фотка № {count}:", chat_id=kwargs["chat_id"])
+                bot.send_photo(chat_id=kwargs["chat_id"], photo=open(file_images_nasa, "rb"))
+                time.sleep(10)
+            bot.send_message(text=f"Ожидание следующей публикации!", chat_id=kwargs["chat_id"])
+            bot.send_message(
+                text=f"Следующая публикация через {kwargs['timeout_user_info']}",
+                chat_id=kwargs["chat_id"],
+            )
+            time.sleep(int(kwargs["timeout"]))
+        except TelegramError as exc:
+            logging.warning(exc)
+
+
+def get_period_from_user():
+    parser = argparse.ArgumentParser(
+        description="The script sends a photo of the planet to"
+                    " the telegram channel with a specified frequency."
+    )
+    parser.add_argument(
+        "-hh", "--hours", help="Set the update period in hours use arguments: '-hh or --hours'"
+    )
+    parser.add_argument(
+        "-s", "--seconds", help="Set the update period in seconds use arguments: '-s or --seconds'"
+    )
+    args_timeout_hours = parser.parse_args().hours
+    args_timeout_seconds = parser.parse_args().seconds
+
+    if args_timeout_hours:
+        return int(args_timeout_hours)
+    if args_timeout_seconds:
+        return float(args_timeout_seconds) / 60 / 60
+    else:
+        return 24
+
+
+def defines_timeout_user():
+    timeout_user = get_period_from_user()
+    timeout = 60 * 60 * timeout_user
+    if (int(timeout_user) > 1) and (float(timeout) / 60 / 60 != 24):
+        print(f"--- Выбрана публикация раз в {timeout_user} часа ---")
+        timeout_user = timeout
+        timeout_user_info = f"{timeout_user} часа"
+    elif float(timeout_user) < 1:
+        print(f"--- Выбрана публикация раз в {int(timeout)} секунд ---")
+        timeout_user = int(timeout)
+        timeout_user_info = f"{int(timeout)} секунд"
+    else:
+        print(f"--- Публикация по умолчанию раз в {timeout_user} часа ---")
+        timeout_user = timeout_user
+        timeout_user_info = f"{timeout_user} часа"
+    return timeout, timeout_user, timeout_user_info
+
+
+def main():
+    logging.basicConfig(
+        level=logging.WARNING,
+        filename="logs.log",
+        filemode="w",
+        format="%(asctime)s - [%(levelname)s] - %(message)s",
+    )
+
+    CHAT_ID = -1001647060957
+    load_dotenv()
+    today = datetime.date.today()
+    timeout, timeout_user, timeout_user_info = defines_timeout_user()
+
+    parameters = {
+        "token": os.getenv("API_KEY_NASA"),
+        "token_bot": os.getenv("API_KEY_BOT"),
+        "dir_name": "NASA_images",
+        "year": f"{today.year}",
+        "month": f"{today.month}",
+        "day": f"{today.day}",
+        "today_date": f"{today}",
+        "url_archive": f"https://api.nasa.gov/EPIC/api/natural/date/"
+                       f"{today.year}-{today.month}-{today.day}",
+        "timeout": timeout,
+        "timeout_user": timeout_user,
+        "timeout_user_info": timeout_user_info,
+        "chat_id": CHAT_ID,
+    }
+
+    publish_photo(**parameters)
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    token = os.getenv("API_KEY_NASA")
-    dirname = "NASA_images"
-    year = "2021"
-    month = "07"
-    day = "03"
-    url_archive = f"https://api.nasa.gov/EPIC/api/natural/date/{year}-{month}-{day}"
-    # fetch_spacex_last_launch(dir_name=dirname)
-
-
+    main()
